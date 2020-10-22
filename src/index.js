592cd70157741283728b6a0e296e95c52e867dc2
Akinori Abe <abe_akinori@cyberagent.co.jp>
2020-10-22 13:33:04 +0900

    :art: fix according to eslint

--- src/index.js
+++ src/index.js
@@ -2,9 +2,9 @@
 import "./assets/images/obs-create-text-source.png";
 import "./assets/images/obs-websocket-connection.png";
 
-import bootstrap from "bootstrap.native";
+import bootstrap from "bootstrap.native"; // eslint-disable-line no-unused-vars
 import * as riot from "riot";
 import HomeView from "./views/HomeView.tag.html";
 
-riot.register('homeview', HomeView);
-riot.mount('homeview');
+riot.register("homeview", HomeView);
+riot.mount("homeview");
